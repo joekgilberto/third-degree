bddf483165175b5dff059ae71207e19bebac56ad
joekgilberto <joekgilberto@gmail.com>
2024-03-09 17:58:08 -0500

    adds ThirdDegreeDatabaseSettings class to models

---
+++ service/Models/ThirdDegreeDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace service.Models
+{
+	public class ThirdDegreeDatabaseSettings
+	{
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string QuizzesCollectionName { get; set; } = null!;
+    }
+}
+
