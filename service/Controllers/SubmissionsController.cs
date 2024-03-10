0554de687373bea73c57d63590a14aa5307edf5c
joekgilberto <joekgilberto@gmail.com>
2024-03-09 20:24:21 -0500

    adds SubmissionsController and submission crud functions

---
+++ service/Controllers/SubmissionsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using service.Models;
+using service.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubmissionsController : ControllerBase
+    {
+        private readonly SubmissionsService _submissionsService;
+
+        public SubmissionsController(SubmissionsService submissionsService)
+        {
+            _submissionsService = submissionsService;
+        }
+
+        [HttpGet]
+        public async Task<List<Submission>> Index()
+        {
+            return await _submissionsService.GetAsync();
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Submission>> Get(string id)
+        {
+            Submission? submission = await _submissionsService.GetByIdAsync(id);
+
+            if (submission is null)
+            {
+                return NotFound();
+            }
+
+            return submission;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Submission newSubmission)
+        {
+            await _submissionsService.CreateAsync(newSubmission);
+
+            return CreatedAtAction(nameof(Get), new { id = newSubmission.Id }, newSubmission);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Submission updatedSubmission)
+        {
+            Submission? submission = await _submissionsService.GetByIdAsync(id);
+
+            if (submission is null)
+            {
+                return NotFound();
+            }
+
+            updatedSubmission.Id = submission.Id;
+
+            await _submissionsService.UpdateAsync(id, updatedSubmission);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Remove(string id)
+        {
+            Submission? submission = await _submissionsService.GetByIdAsync(id);
+
+            if (submission is null)
+            {
+                return NotFound();
+            }
+
+            await _submissionsService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
+
