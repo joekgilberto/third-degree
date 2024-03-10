17741ba31f9fc4e13c2f30a4ab3eb2f5deef6498
joekgilberto <joekgilberto@gmail.com>
2024-03-09 20:21:57 -0500

    adds CategoriesController and categories crud functions

--- service/Controllers/CategoriesController.cs
+++ service/Controllers/CategoriesController.cs
@@ -1,19 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using service.Models;
+using service.Services;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace service.Controllers
 {
-    public class CategoriesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly CategoriesService _categoriesService;
+
+        public CategoriesController(CategoriesService categoriesService)
         {
-            return View();
+            _categoriesService = categoriesService;
+        }
+
+        [HttpGet]
+        public async Task<List<Category>> Index()
+        {
+            return await _categoriesService.GetAsync();
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Category>> Get(string id)
+        {
+            Category? category = await _categoriesService.GetByIdAsync(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Category newCategory)
+        {
+            await _categoriesService.CreateAsync(newCategory);
+
+            return CreatedAtAction(nameof(Get), new { id = newCategory.Id }, newCategory);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Category updatedCategory)
+        {
+            Category? category = await _categoriesService.GetByIdAsync(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            updatedCategory.Id = category.Id;
+
+            await _categoriesService.UpdateAsync(id, updatedCategory);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Remove(string id)
+        {
+            Category? category = await _categoriesService.GetByIdAsync(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            await _categoriesService.RemoveAsync(id);
+
+            return NoContent();
         }
     }
 }
