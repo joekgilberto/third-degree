db6af34e1832f4ea44b422a081a7b68d14e3abfc
joekgilberto <joekgilberto@gmail.com>
2024-03-10 21:14:10 -0400

    adds placeholder text to Home

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -6,9 +6,11 @@ export default function Nav() {
   return (
     <nav>
       <h1>Third<br/>Degree</h1>
+      <hr />
       <button>Home</button>
       <button>Categories</button>
       <button>Build a Quiz</button>
+      <button>Login</button>
     </nav>
   );
 }
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,7 +5,10 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="Home">
-      <h2>Home</h2>
+      <h2>A challenger approaches!</h2>
+      <p>Welcome to <span className='bold'>Third Degree</span>, the quiz center of the known universe!<br />Here at Third Degree, we believe in the power of knowledge and the joy of learning. Whether you're here to challenge your intellect or to craft thought-provoking quizzes, you've found the perfect digital institution.<br />Let your quest for the Third Degree begin!</p>
+      <h3>Our Hardest Quizzes:</h3>
+      <hr />
     </div>
   );
 }
