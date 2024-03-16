e993d82975d94cfd6f2f8e8c61e2ddafb060d527
joekgilberto <joekgilberto@gmail.com>
2024-03-16 16:07:36 -0400

    adds links to hardest quizzes

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,6 +5,7 @@ import * as quizServices from '../../utilities/quiz/quiz-services';
 import { Quiz } from '../../utilities/types';
 
 import QuizCard from '../../components/QuizCard/QuizCard';
+import { Link } from 'react-router-dom';
 
 const dummyData: Array<Quiz> = [
   {
@@ -61,17 +62,17 @@ export default function Home() {
 
   const [hardestQuizzes, setHardestQuizzes] = useState<Array<Quiz>>([]);
 
-  async function handleRequest(){
-    await quizServices.getHardestQuizzes().then((quizzes)=>{
+  async function handleRequest() {
+    await quizServices.getHardestQuizzes().then((quizzes) => {
       setHardestQuizzes(quizzes);
     });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     handleRequest();
-  },[])
+  }, [])
 
-  if(!hardestQuizzes.length){
+  if (!hardestQuizzes.length) {
     return <p>Loading...</p>
   }
 
@@ -81,8 +82,12 @@ export default function Home() {
       <p>Welcome to <span className='bold'>Third Degree</span>, the quiz center of the known universe!<br />Here at Third Degree, we believe in the power of knowledge and the joy of learning. Whether you're here to challenge your intellect or to craft thought-provoking quizzes, you've found the perfect digital institution.<br />Let your quest for the Third Degree begin!</p>
       <h3>Our Hardest Quizzes:</h3>
       <hr />
-      {hardestQuizzes.map((quiz: Quiz)=>{
-        return <QuizCard key={quiz.id} quiz={quiz} />
+      {hardestQuizzes.map((quiz: Quiz) => {
+        return (
+          <Link to={`/quiz/${quiz.id}`}>
+            <QuizCard key={quiz.id} quiz={quiz} />
+          </Link>
+        )
       })}
     </div>
   );
