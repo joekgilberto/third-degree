27bf600045c7751e7f09467c62883136bdfe6a22
joekgilberto <joekgilberto@gmail.com>
2024-03-18 15:35:16 -0400

    restructures Nav divs and adds logo img

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -5,20 +5,25 @@ import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <nav>
-      <h1>Third<br />Degree</h1>
+    <header>
+      <div className='logo'>
+        <img src='https://i.imgur.com/wcVN8CX.png' />
+        <h1>Third Degree</h1>
+      </div>
       <hr />
-      <Link to='/'>
-        <button>Home</button>
-      </Link>
-      <Link to='/categories'>
-        <button>Categories</button>
-      </Link>
-      <Link to='/quiz/new'>
-        <button>Build a Quiz</button>
-      </Link>
-      {/* TODO: Toggle Login to profile link when logged in */}
-      <button>Login</button>
-    </nav>
+      <nav>
+        <Link to='/'>
+          <button>Home</button>
+        </Link>
+        <Link to='/categories'>
+          <button>Categories</button>
+        </Link>
+        <Link to='/quiz/new'>
+          <button>Build a Quiz</button>
+        </Link>
+        {/* TODO: Toggle Login to profile link when logged in */}
+        <button>Login</button>
+      </nav>
+    </header>
   );
 }
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -10,10 +10,12 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 
   const [hardestQuizzes, setHardestQuizzes] = useState<Array<Quiz>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   async function handleRequest() {
     await quizServices.getHardestQuizzes().then((quizzes) => {
       setHardestQuizzes(quizzes);
+      setIsLoading(false);
     });
   };
 
@@ -21,23 +23,25 @@ export default function Home() {
     handleRequest();
   }, [])
 
-  if (!hardestQuizzes.length) {
-    return <p>Loading...</p>
-  }
-
   return (
     <div className='Home'>
       <h2>A challenger approaches!</h2>
       <p>Welcome to <span className='bold'>Third Degree</span>, the quiz center of the known universe!<br />Here at Third Degree, we believe in the power of knowledge and the joy of learning. Whether you're here to challenge your intellect or to craft thought-provoking quizzes, you've found the perfect digital institution.<br />Let your quest for the Third Degree begin!</p>
       <h3>Our Hardest Quizzes:</h3>
       <hr />
-      {hardestQuizzes.map((quiz: Quiz) => {
-        return (
-          <Link to={`/quiz/${quiz.id}`}>
-            <QuizCard key={quiz.id} quiz={quiz} />
-          </Link>
-        )
-      })}
+      {isLoading ?
+        <p>Loading...</p>
+        :
+        hardestQuizzes.length ?
+          hardestQuizzes.map((quiz: Quiz) => {
+            return (
+              <Link to={`/quiz/${quiz.id}`}>
+                <QuizCard key={quiz.id} quiz={quiz} />
+              </Link>
+            )
+          })
+          :
+          <p>No quizzes yet- start building!</p>}
     </div>
   );
 }
