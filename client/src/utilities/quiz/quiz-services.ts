83fd443672d7d9a7a52ca4c912bcd69aa472b467
joekgilberto <joekgilberto@gmail.com>
2024-03-20 17:57:31 -0400

    deletes related submissions and removes submissions from user submissions when a quiz is deleted

--- client/src/utilities/quiz/quiz-services.ts
+++ client/src/utilities/quiz/quiz-services.ts
@@ -1,5 +1,7 @@
 import * as quizzesApi from './quiz-api';
-import { Quiz } from '../types';
+import * as submissionServices from '../submission/submission-services';
+import * as userServices from '../user/user-services';
+import { Quiz, Submission, User } from '../types';
 
 export async function getAllQuizzes() {
     try {
@@ -19,19 +21,19 @@ export async function getHardestQuizzes() {
                 return 0;
             } else if (!a.avgScore) {
                 return 1;
-            } else if (!b.avgScore){
+            } else if (!b.avgScore) {
                 return -1;
             } else {
-                return a.avgScore  - b.avgScore;
+                return a.avgScore - b.avgScore;
             };
         });
 
-        if (res.data.length > 5){
-            return res.data.slice(0,5);
+        if (res.data.length > 5) {
+            return res.data.slice(0, 5);
         };
 
         return res.data;
-        
+
     } catch (err) {
         return err;
     }
@@ -39,7 +41,7 @@ export async function getHardestQuizzes() {
 
 export async function getQuiz(id: string | undefined) {
     try {
-        if(!id){
+        if (!id) {
             throw Error('Error: id undefined.');
         }
         const res = await quizzesApi.show(id);
@@ -51,7 +53,7 @@ export async function getQuiz(id: string | undefined) {
 
 export async function getQuizByCategory(id: string | undefined) {
     try {
-        if(!id){
+        if (!id) {
             throw Error('Error: id undefined.');
         }
         const res = await quizzesApi.byCategory(id);
@@ -73,7 +75,7 @@ export async function createQuiz(data: Quiz) {
 
 export async function updateQuiz(id: string | undefined, data: Quiz) {
     try {
-        if(!id){
+        if (!id) {
             throw Error('Error: id undefined.');
         }
         const res = await quizzesApi.update(id, data);
@@ -85,12 +87,36 @@ export async function updateQuiz(id: string | undefined, data: Quiz) {
 
 export async function destroyQuiz(id: string | undefined) {
     try {
-        if(!id){
+        if (!id) {
             throw Error('Error: id undefined.');
         }
-        const res = await quizzesApi.destroy(id);
-        return res.data;
+        const res = await quizzesApi.destroy(id).then(async () => {
+            await submissionServices.getAllSubmissions().then(async (submissions: Array<Submission>) => {
+                if (submissions.length) {
+                    const quizSubmissions = submissions.filter((submission: Submission) => {
+                        return submission.quiz === id;
+                    })
+                    for (let submission of quizSubmissions) {
+                        await userServices.getUser(submission.challenger).then(async (user: User) => {
+                            if (submission.id) {
+
+                                const userSubmissions: Array<string> = [...user.submissions];
+                                const idx: number = userSubmissions.indexOf(submission.id);
+                                if (idx !== -1){
+                                    userSubmissions.splice(idx,1);
+                                    await userServices.addSubmission(user.id, userSubmissions);
+                                }
+                            }
+                        })
+                        await submissionServices.destroySubmission(submission.id)
+
+                    }
+                }
+            })
+        });
+
+        return {};
     } catch (err) {
-        return err
+        return err;
     }
 }
